6a71be73c15013ad2d61282dc9742dbf435376b1
Faez-B <faezbhatti@gmail.com>
2022-05-23 14:35:20 +0200

    KO : Test non fonctionnels Exécution très longue

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -16,4 +16,23 @@ describe('Section 1', () => {
 
     //     expect(db.memoryDb.get(1)).toEqual(obj);
     // })
+    // it("should not return any object", async (obj) => {
+    //     const result = await request(app)
+    //         .get("/tache/1000")
+    //         .expect(404);
+
+    //     expect(db.memoryDb.get(1)).not.toEqual(obj);
+    // })
+
+    
+    // it("should return inserted object", async (obj) => {
+    //     const inserted = { description: "New Tache", faite : false};
+
+    //     const result = await request(app)
+    //         .post("/taches")
+    //         .send(inserted)
+    //         .expect(201);
+
+    //     expect(db.memoryDb.get(id)).toMatchObject(inserted);
+    // })
 })
